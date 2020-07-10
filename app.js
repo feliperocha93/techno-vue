0e223de96650294e5ca007c0de909f8dfa3e2bc6
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-07-10 12:32:19 -0300

    Criação e estilização do modal para exibir produto ao toque

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
+    produto: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
         .then((produtos) => produtos.json())
         .then((produtos) => (this.produtos = produtos));
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((produto) => produto.json())
+        .then((produto) => (this.produto = produto));
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
     },
   },
   created() {
